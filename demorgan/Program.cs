3d6c5984e2a7c86ab15d8f354662a07b6dbf97cd
bettina-thomas <73313968+bettina-thomas@users.noreply.github.com>
2020-11-19 16:49:12 -0600

    Add files via upload

--- demorgan/Program.cs
+++ demorgan/Program.cs
@@ -1,132 +1,101 @@
 ﻿using System;
 using LogicCircuit;
+//#pragma warning restore CS1634 // Expected 'disable' or 'restore' after #pragma warning
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace demorgan
+namespace circuit
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ored then Notted");
+            Console.WriteLine("Hello World!");
             var inputs = new List<IdentityInput>();
-            //var orgate = new OR();
-            inputs.Add(new IdentityInput() { X = false, Y = false });
-            inputs.Add(new IdentityInput() { X = false, Y = true });
-            inputs.Add(new IdentityInput() { X = true, Y = false });
-            inputs.Add(new IdentityInput() { X = true, Y = true });
+            inputs.Add(new IdentityInput() { A = false, D = false, X = false });
+            inputs.Add(new IdentityInput() { A = false, D = false, X = false });
+            inputs.Add(new IdentityInput() { A = false, D = true, X = true });
+            inputs.Add(new IdentityInput() { A = false, D = true, X = true });
+            inputs.Add(new IdentityInput() { A = true, D = false, X = true });
+            inputs.Add(new IdentityInput() { A = true, D = false, X = false });
+            inputs.Add(new IdentityInput() { A = true, D = true, X = true });
+            inputs.Add(new IdentityInput() { A = true, D = true, X = true });
 
+            /*var notGate = new NOT();
+            notGate.SetInputA(true);
+            notGate.SetInputA(false);
+            bool state = notGate.Output.State;
+            var orGate = new OR();
+            orGate.SetInputA(state);
+            orGate.SetInputA(false);
+            orGate.SetInputB(state);
+            orGate.SetInputB(false);
+            var andGate = new AND();*/
 
-            foreach (var tes in inputs)
+            foreach (var item in inputs)
             {
-                //var identity1 = new Identity();
-                var identity1 = new Identity();
-                var identity2 = new Identity();
+                var identity = new Identity();
+                identity.SetInputD = item.D;
+                identity.SetInputA = item.A;
+                identity.SetInputX = item.X;
 
-                identity1.SetInputX = tes.X;
-                identity2.SetInputY = tes.Y;
-
-                string result = string.Join(",", identity1.SetInputX, identity2.SetInputY);
-                new OR(result);
-                Console.WriteLine($"RESULT: {result}");
-                if (identity1.SetInputX == true)
-                {
-                    Console.WriteLine(false);
-                }
-                else if (identity2.SetInputY == true)
-                {
-                    Console.WriteLine(false);
-                }
-                else
-                {
-                    Console.WriteLine(true);
-                }
+                var output = identity.Validate();
+                Console.WriteLine($" D = {identity.SetInputD }," +
+                    $" A = {identity.SetInputA}, " +
+                   $" X= { identity.SetInputX}, " +  $"OutPut = {output}"
+                 );
             }
-            Console.WriteLine("results after notting variable and anding");
-            foreach (var tes2 in inputs)
-            {
-                var identity1 = new Identity();
-                var identity2 = new Identity();
-
-                identity1.SetInputX = tes2.X;
-                identity2.SetInputY = tes2.Y;
-
-                string result = string.Join(",", identity1.SetInputX, identity2.SetInputY);
-                new AND(result);
-                Console.WriteLine($"RESULT2: {result}");
-                if (identity1.SetInputX != identity2.SetInputY)
-                {
-                    Console.WriteLine(false);
-                }
-                else if(identity1.SetInputX == true)
-                {
-                    Console.WriteLine(false);
-                }
-                else { Console.WriteLine(true); }
-            }
-            Console.WriteLine("RESULT == RESULT2. Proves demorgan's theorem" + "\n\n");
-            Console.WriteLine("Anded and then Notted");
-            foreach (var tes3 in inputs)
-            {
-                var identity1 = new Identity();
-                var identity2 = new Identity();
-
-                identity1.SetInputX = tes3.X;
-                identity2.SetInputY = tes3.Y;
-
-                string result = string.Join(",", identity1.SetInputX, identity2.SetInputY);
-                new AND(result);
-                Console.WriteLine($"RESULT3: {result}");
-                if (identity1.SetInputX != identity2.SetInputY)
-                {
-                    Console.WriteLine(true);
-                }
-                else if (identity1.SetInputX == true)
-                {
-                    Console.WriteLine(false);
-                }
-                else { Console.WriteLine(true); }
-            }
-            Console.WriteLine("Not each variable then add");
-            foreach (var tes4 in inputs)
-            {
-                var identity1 = new Identity();
-                var identity2 = new Identity();
-
-                identity1.SetInputX = tes4.X;
-                identity2.SetInputY = tes4.Y;
-
-                string result = string.Join(",", identity1.SetInputX, identity2.SetInputY);
-                new OR(result);
-                Console.WriteLine($"RESULT4: {result}");
-                if (identity1.SetInputX != identity2.SetInputY)
-                {
-                    Console.WriteLine(true);
-                }
-                else if (identity1.SetInputX == true)
-                {
-                    Console.WriteLine(false);
-                }
-                else { Console.WriteLine(true); }
-            }
-            Console.WriteLine("RESULT3 == RESULT4. Proves demorgan's theorem");
         }
-        class IdentityInput
+         public class IdentityInput
         {
+            public bool D { get; set; }
+            public bool A { get; set; }
             public bool X { get; set; }
-            public bool Y { get; set; }
 
         }
-        class Identity
+        public class Identity
         {
+            public bool SetInputD { get; set; }
+            public bool SetInputA { get; set; }
             public bool SetInputX { get; set; }
-            public bool SetInputY { get; set; }
             
+            public bool Validate()
+            {
+                var notGate = new NOT();
+                notGate.SetInputA(true);
+                notGate.SetInputA(false);
+                bool state = notGate.Output.State;
+                var andGate = new AND();
+                var andGate2 = new AND();
+                andGate.SetInputA(state);
+                andGate2.SetInputA(state);
+                andGate.SetInputB(state);
+                andGate2.SetInputB(state);
+
+
+                var result = andGate.Output.State;
+                var result2 = andGate2.Output.State;
+                var orGate = new OR();
+                var orGate1 = new OR();
+                orGate.SetInputA(result);
+                orGate.SetInputB(result);
+                orGate1.SetInputA(result2);
+                orGate1.SetInputB(result2);
+                
+                var newstate = orGate.Output.State;
+                var newstate1 = orGate1.Output.State;
+                var orlast = new OR();
+                orlast.SetInputA(newstate);
+                orlast.SetInputB(newstate1);
+
+                var finalresult = orlast.Output.State;
+                return finalresult;
+
+            }
         }
-       
+
     }
-    
 }
